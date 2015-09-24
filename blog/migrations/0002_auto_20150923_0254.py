2c1a3a010406f1a2429e6fa387870094ef2a5c07
hector <hectorm@mesoamericana.edu.gt>
2015-09-23 18:13:03 -0600

    mi aplicacion

---
+++ blog/migrations/0002_auto_20150923_0254.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Articulo',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True, verbose_name='ID', auto_created=True)),
+                ('titulo', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('fecha_creacion', models.DateTimeField(default=django.utils.timezone.now)),
+                ('fecha_publicacion', models.DateTimeField(blank=True, null=True)),
+                ('autor', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='post',
+            name='author',
+        ),
+        migrations.DeleteModel(
+            name='Post',
+        ),
+    ]
