e9b699ede8bc47139436bd7af169b45484254f4f
hector <hectorm@mesoamericana.edu.gt>
2015-09-30 19:05:47 -0600

    se creo publicaciones

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import post 
+from django .utils import timezone
 
 # Create your views here.
 def listar_publicaciones(request):
-    return render(request, 'blog/listar_publicaciones.html', {$})
+    publicaciones = post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/listar_publicaciones.html', {'publicaciones':publicaciones})
