a0b9fe35fbda9f7310d51afdfa1199ce5df65107
hector <hectorm@mesoamericana.edu.gt>
2015-10-29 19:55:04 -0600

    as

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Articulo
 from django .utils import timezone
 
@@ -6,3 +6,7 @@ from django .utils import timezone
 def listar_publicaciones(request):
     publicaciones = Articulo.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_publicaciones.html', {'publicaciones':publicaciones})
+
+def post_detail(request, pk):
+    publicaciones = get_object_or_404(Articulo, pk=pk)
+    return render(request, 'blog/post_detail.html', {'publicaciones': publicaciones})
